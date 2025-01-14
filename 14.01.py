01648faebf365397284d07c3be4717834e192bcf
Melanka44 <kmelania272@gmail.com>
2025-01-14 17:34:39 +0200

    Add files via upload

---
+++ 14.01.py
@@ -0,0 +1,9 @@
+# Метод 1: str.partition()
+text = "Python є чудовою мовою для програмування."
+result = text.partition("чудовою")
+print(result)
+
+# Метод 2: str.zfill()
+number = "42"
+result = number.zfill(5)
+print(result)
